3a858277261c72ff278baff483e082ed2bf41feb
rajanant49 <60772854+rajanant49@users.noreply.github.com>
2021-01-16 16:25:05 +0530

    trying with cv2

--- main.py
+++ main.py
@@ -96,16 +96,16 @@ plt.ylabel("Principal Component 2")
 plt.colorbar()
 
 st.pyplot(fig)
-# def load_image(image_file):
-# 	img = Image.open(image_file)
-# 	return img
-#
-# image_file = st.file_uploader("Upload Image",type=['png','jpeg','jpg'])
-# if image_file is not None:
-#     file_details = {"Filename":image_file.name,"FileType":image_file.type,"FileSize":image_file.size}
-#     st.write(file_details)
-#
-#     img = load_image(image_file)
-#     st.image(img,width=250,height=250)
-#     image_array=np.asarray(img)
-#     st.image(image_array,width=100,height=100)
+def load_image(image_file):
+	img = Image.open(image_file)
+	return img
+
+image_file = st.file_uploader("Upload Image",type=['png','jpeg','jpg'])
+if image_file is not None:
+    file_details = {"Filename":image_file.name,"FileType":image_file.type,"FileSize":image_file.size}
+    st.write(file_details)
+
+    img = load_image(image_file)
+    st.image(img,width=250,height=250)
+    image_array=np.asarray(img)
+    st.image(image_array,width=100,height=100)
